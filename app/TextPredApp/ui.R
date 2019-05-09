9924ccac81be8fb1b3a095b09b4232fe4601200c
Jakub <wiatrak667@op.pl>
2019-05-09 12:23:44 +0200

    style

--- app/TextPredApp/ui.R
+++ app/TextPredApp/ui.R
@@ -15,23 +15,28 @@ shinyUI(fluidPage(theme = shinytheme("superhero"),
 
             #text_input {background-color:#ABB6C2;
                         font-weight: bold;}
+
+            .shiny-input-container:not(.shiny-input-container-inline) {
+                width: auto;
+            }
         "))
     ),            
 
     fluidRow(
-        column(6, offset = 1,
+        column(6,
             # Application title
             headerPanel("SBO Text Predictor"),
             # first row - large text input
-            helpText("Just start typing to see the predictions at the bottom. Click on a prediction to use it in your text"),
-            textAreaInput("text_input", "", value="", width = "49vw", height = "300px"),
+            helpText("Just start typing to see the predictions at the bottom. Click on a prediction to use it in your text."),
+            fluidRow(
+                textAreaInput("text_input", "", value="", width = "100%", height = "300px")),
         # second row - three best predictions
             fluidRow(
                 div(style="display:inline-block;width:32%;text-align: center;",uiOutput("prediction2_button")),
                 div(style="display:inline-block;width:32%;text-align: center;",uiOutput("prediction1_button")),
                 div(style="display:inline-block;width:32%;text-align: center;",uiOutput("prediction3_button"))
-                   )
-        )
+                   ),
+        style = "float: none; margin: 0 auto")
     )
     
 ))
